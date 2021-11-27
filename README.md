1e492b8144b3a3462b84ee2948e3ae3290510584
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-11-27 20:15:28 +0600

    redmi file edited

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Getting Started with med-life-health-care application...
 
-I have showed some department of an  hospital/health care service..I have also showed some of the doctors images..I have added log in and registration form for the app..
+● Related to a health care service. A front-end web application created with React js.
+● Users can see different health care services. To see details of any service user must log in.
+● Users can login with email or google login.
 
 ### Sign in method
 
@@ -10,7 +12,8 @@ I have added google sign in method in this app..
 
 I have used Firebase for deployment of the app.Here is the live link of my app: https://med-life-health-care.web.app/
 
-### Context API 
+### Context API
+
 I have used context API in this app for using anywhere of the app...And for syncronized Auth..
 
 ### Framework
@@ -20,5 +23,3 @@ I have used Bootstrap and react Bootstrap as css framework for this app..
 ### Route
 
 I have used Browser router as Router..I have declared some route of the app in App.js..
-
-
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -2,55 +2,76 @@ import React from 'react';
 import { Button, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
-import logo from "../../../logo.png";
+import logo from '../../../logo.png';
 import './Footer.css';
 
 const Footer = () => {
-const {user,logOut} = useAuth();
+   const { user, logOut } = useAuth();
 
-    return (
-        <div className="footer m-0">
+   return (
+      <div className="footer m-0">
+         <div className="py-3 mt-5">
+            <div className="row m-0">
+               <div className="col-md-4 text-white">
+                  <img src={logo} alt="" />
+                  <p className="py-3 px-5">
+                     The fundamental purpose of health care is to enhance
+                     quality of life by enhancing health. Commercial businesses
+                     focus on creating financial profit to support their
+                     valuation and remain viable. Health care must focus on
+                     creating social profit to fulfill its promise to society.
+                  </p>
+               </div>
 
-            <div className="py-3 mt-5">
-                <div className="row m-0">
-                    <div className="col-md-4 text-white">
-                        <img src={logo} alt="" />
-                        <p className="py-3 px-5">The fundamental purpose of health care is to enhance quality of life by enhancing health. Commercial businesses focus on creating financial profit to support their valuation and remain viable. Health care must focus on creating social profit to fulfill its promise to society.</p>
-                    </div>
+               <div className="col-md-4 links py-2">
+                  <Nav.Link as={Link} to="/home">
+                     Home
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/services">
+                     Services
+                  </Nav.Link>
+                  <Nav.Link as={Link} to="/doctors">
+                     Doctors
+                  </Nav.Link>
+                  {user?.email ? (
+                     <Button onClick={logOut} className="btn btn-light mb-3">
+                        Logout
+                     </Button>
+                  ) : (
+                     <Nav.Link as={Link} to="/login">
+                        Login
+                     </Nav.Link>
+                  )}
+                  <br />
+                  <Navbar.Text className="bg-white px-3">
+                     Signed in as: <a href="#login">{user?.displayName}</a>
+                  </Navbar.Text>
+               </div>
 
-                    <div className="col-md-4 links py-2">
-
-                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/services">Services</Nav.Link>
-                        <Nav.Link as={Link} to="/doctors">Doctors</Nav.Link>
-                        {user?.email ?
-                        <Button onClick={logOut} className="btn btn-light mb-3">Logout</Button> :
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>}
-                        <br />
-                        <Navbar.Text className="bg-white px-3">
-                            Signed in as: <a href="#login">{user?.displayName}</a>
-                        </Navbar.Text>
-
-                    </div>
-
-                    <div className="col-md-4 text-white">
-                        <h2>Contacts</h2>
-                        <h5>Call Now</h5>
-                        <h6>0 (800) 716 95 25</h6>
-                        <h5>Send Mail</h5>
-                        <h6>medlife24@email.com</h6>
-                        <h5>Visit MedLife</h5>
-                        <h6 className="text-warning">8500, E Street, New York, NY, 75030</h6>
-
-                    </div>
-                </div>
+               <div className="col-md-4 text-white">
+                  <h2>Contacts</h2>
+                  <h5>Call Now</h5>
+                  <h6>0 (800) 716 95 25</h6>
+                  <h5>Send Mail</h5>
+                  <h6>medlife24@email.com</h6>
+                  <h5>Visit MedLife</h5>
+                  <h6 className="text-warning">
+                     8500, E Street, New York, NY, 75030
+                  </h6>
+               </div>
             </div>
+         </div>
 
-            <div>
-                <p className="text-light"><small>MedLife © 2021. All rights reserved. Terms of use and Privacy Policy</small></p>
-            </div>
-        </div>
-    );
+         <div className="py-1">
+            <p className="text-light">
+               <small>
+                  MedLife © 2021. All rights reserved. Terms of use and Privacy
+                  Policy
+               </small>
+            </p>
+         </div>
+      </div>
+   );
 };
 
 export default Footer;
